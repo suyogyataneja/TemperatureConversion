f0cab3dceb8a5e31309f045a26839d3442f0a5f0
Taneja3375 <suyogyataneja@gmail.com>
2021-07-07 08:20:21 +1000

    Updated the app.component ts and html file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {Subject } from 'rxjs';
+import { TemperatureComponent } from './temperature/temperature.component';
 
 
 @Component({
@@ -8,11 +9,36 @@ import {Subject } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild(TemperatureComponent) tempVariable : TemperatureComponent;
+
   title = 'temp';
   parentText:any;
   newValue: String;
   CelciusParentText:any;// pass this value to the child component
+childTextBoxvalue:any;
 
+  celcius_conversion(){
+
+    this.tempVariable.celcius_to_fahrenheit();
+  }
+
+  getvalue(val){
+    val =
+    console.log(val);
+    
+  }
+  updateValue(event){
+    this.childTextBoxvalue=event;
+  }
+  increase(){
+
+    this.tempVariable.increasebyone();
+  }
+
+  decrease(){
+
+    this.tempVariable.decreasebyone();
+  }
   // parentClick: Subject<void> = new Subject<void>();
   // public name= "36 C";
 
--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -14,22 +14,50 @@ export class TemperatureComponent implements OnInit {
 
   @Input('parentvalue')  parentvaluetype:any;
   @Input('ConversionParentValue') celciusparentvalue:any;
+// @Output() Conversionvalue: EventEmitter<any> = new EventEmitter<any>();
+
   // @Input() props: { parentvaluetype:any;  celciusparentvalue:any };
 
   // @Input() parentClick:Subject<void>;
-  // @Output('ontextfromchild') passtoParent: EventEmitter<string> = new EventEmitter<string>();
+  @Output('ontextfromchild') passtoParent: EventEmitter<string> = new EventEmitter<string>();
   // childtextbox:string;
 
+  message:string='';
+  count:number=0;
+  temp_value:number;
+  celcius_output:any;
+  childtextbox:any;
   constructor() { }
-
+  // (parentvaluetype-32)*5/9
   ngOnInit(): void {
   
   }
 
-  // passValuetoParent(){
-  //   this.passtoParent.emit(this.childtextbox);
+  celcius_to_fahrenheit(){
+    this.parentvaluetype = ((this.parentvaluetype-32)*5/9);
+    this.celcius_output ="Celcius to fahrenheit : " + this.temp_value;
+  }
+  increasebyone(){
 
-  // }
+    
+    this.count=this.count+1;
+    this.message="counter :" + this.count;
+  }
+
+  decreasebyone(){
+
+    this.count=this.count-1;
+    this.message="counter :" + this.count;
+  }
+  passValuetoParent(){
+    this.passtoParent.emit(this.childtextbox);
+
+  }
+  getvalue(val){
+    
+    console.log(val);
+    
+  }
 
   
 }
